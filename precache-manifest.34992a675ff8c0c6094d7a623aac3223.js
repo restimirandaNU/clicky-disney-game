efcbd1fbbd921353ac0ff86eacf63d0842b61dc8
Resti Miranda <restimiranda@restis-mbp.attlocal.net>
2019-03-10 19:23:14 -0500

    Updates

---
+++ precache-manifest.34992a675ff8c0c6094d7a623aac3223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a09049bbcb1619815c63",
+    "url": "/clicky-disney-game/static/js/runtime~main.ba3c3638.js"
+  },
+  {
+    "revision": "5ace0150c98f1741e871",
+    "url": "/clicky-disney-game/static/js/main.a100d26a.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/clicky-disney-game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "5ace0150c98f1741e871",
+    "url": "/clicky-disney-game/static/css/main.7cd59749.chunk.css"
+  },
+  {
+    "revision": "4ae33a13dd8d7b79460e4dd810bbda3c",
+    "url": "/clicky-disney-game/index.html"
+  }
+];
